0b99a5280909839626b4310b0c76238de153d199
AnggaRafi <85323173+AnggaRafi@users.noreply.github.com>
2023-11-16 19:20:21 +0700

    Add files via upload

---
+++ WebApplication1/Data/UserStore.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication1.Models.Dto;
+
+namespace WebApplication1.Data
+{
+    public static class UserStore
+    {
+        public static List<UserDTO> userList = new List<UserDTO>
+        {
+             new UserDTO{Id=1, Username="admin", Password="admin"},
+             new UserDTO{Id=2, Username="raf", Password="12345"}
+        };
+
+    }
+}
